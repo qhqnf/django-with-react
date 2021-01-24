b13438a44f75ea50c0c91c0928c11b36b93a8cd1
yhc <didguscjf95@naver.com>
2021-01-24 21:22:52 +0900

    configure frontend settings and create first page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import './App.scss';
+import React from "react";
+import 'App.scss';
+import PostList from "PostList";
 
 function App() {
   return (
     <div>
       <h1>Hello world!</h1>
+      <PostList />
     </div>
   );
 }
